087ddb1a9c7f6da8225607a09de754100a37c269
erk52 <erk52@cornell.edu>
2017-02-07 13:41:06 -0600

    Add files via upload

--- web/decline.js
+++ web/decline.js
@@ -6,6 +6,7 @@ var CUR_DECLENS = 2;
 var SUCCESS = 0;
 var ATTEMPTS = 0;
 
+var caseNames = {'nom_s':"Nominative Singular", 'gen_s': "Genitive Singular", 'acc_s':"Accusative Singular", 'dat_s':"Dative Singular",'abl_s':"Ablative Singular", 'nom_p':"Nominative Plural", 'gen_p':"Genitive Plural", 'acc_p': "Accusative Plural", 'dat_p':"Dative Plural",'abl_p':"Ablative Plural"}
 
 function compareWords(guessed, correct){
   return guessed.toUpperCase() == correct.toUpperCase()
@@ -15,7 +16,7 @@ function compareWords(guessed, correct){
 
 
 $(document).ready(function(){
-	//newWord();
+	newWord();
 	$("#submit").click(function(){
 		evaluate();
 		newWord();
@@ -50,7 +51,7 @@ $(document).ready(function(){
 		var decs = []
 		for (i=1;i<=5;i++){
 			var but = "#dec_" + i;
-			if $(but).hasClass('btn-success'){
+			if ($(but).hasClass('btn-success')){
 				decs.push(i);
 			}
 			
@@ -60,9 +61,39 @@ $(document).ready(function(){
 		}
 		return decs;
 	}
-
 	
-	$('.btn').on('click', function(){
+	function newWord(){
+		var cases = getCases();
+		var decs = getDeclensions();
+
+		var declension = decs[Math.floor(Math.random() * decs.length)]
+		var myCase = cases[Math.floor(Math.random() * cases.length)]
+		console.log("Case: " + myCase + " and decl: " + declension);
+		
+		var possible_words = Object.keys(NOUN_DB[declension]);
+		console.log("got poss words");
+		CUR_WORD = possible_words[Math.floor(Math.random() * possible_words.length)];
+		CUR_ANSWER = NOUN_DB[declension][CUR_WORD][myCase];
+		
+		$("#word").html(CUR_WORD + ', ' + NOUN_DB[declension][CUR_WORD]['Gender'] + '.: <em>'+NOUN_DB[declension][CUR_WORD]['Definition'] + '</em>');
+		$("#case").html(caseNames[myCase]);
+		
+		
+	}
+	function evaluate(){
+		ATTEMPTS += 1;
+		var cur_guess = $("#guess").val();
+		if (compareWords(cur_guess, CUR_ANSWER)){
+			$('#response').html("<span class='win'>Correct!  "  + CUR_ANSWER + "</span>");
+			SUCCESS += 1;
+		}
+		else{
+			$('#response').html("<span class='lose'>Incorrect: " +CUR_ANSWER + "</span>");
+		}
+		$('#score').html("Current Score: " + SUCCESS +'/' + ATTEMPTS);
+	}
+	
+	$('.myb').on('click', function(){
 		$(this).toggleClass("btn-success");
 		$(this).toggleClass("btn-danger");
 	})
